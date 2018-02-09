6fcc975673b03f0535434000f33f1076331d86af
adrianbermejo <adrianbermejogon@hotmail.com>
2018-02-09 09:02:22 +0100

    primer ejercicio acepta para finalizar con espacios y mayusculas

--- SupportSystem.java
+++ SupportSystem.java
@@ -39,7 +39,7 @@ public class SupportSystem
         while(!finished) {
             String input = reader.getInput();
 
-            if(input.startsWith("bye")) {
+            if(input.trias().startsWith("bye")) {
                 finished = true;
             }
             else {
